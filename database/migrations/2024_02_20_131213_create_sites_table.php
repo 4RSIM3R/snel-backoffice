17f9a75a3c71c891c1dc0bc2493152f6672384fa
ilzam <ilzammulkhaq85@gmail.com>
2024-02-20 20:27:22 +0700

    feat: add base customer related service

---
+++ database/migrations/2024_02_20_131213_create_sites_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Customer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('address');
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 11, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sites');
+    }
+};
